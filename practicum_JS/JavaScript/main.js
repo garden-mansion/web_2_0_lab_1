b1e30fecc86b704b6455d56ab8bb557be35447cb
vstaff <vstaff22b@gmail.com>
2026-02-27 14:12:07 +1000

    feat(ui): Форма для сортировки - не весь функционал.

--- practicum_JS/JavaScript/main.js
+++ practicum_JS/JavaScript/main.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 const form = document.getElementById('filter');
+const sort = document.getElementById('sort');
 
 document.getElementById('find-filtered-items-button').addEventListener('click', function () {
   filterTable(buildings, 'list', form)
@@ -10,4 +11,77 @@ document.getElementById('find-filtered-items-button').addEventListener('click', 
 
 document.getElementById('clear-filter-button').addEventListener('click', function () {
   clearFilter(form, 'list');
+})
+
+// формирование полей элемента списка с заданным текстом и значением
+
+const createOption = (str, val) => {
+  let item = document.createElement('option');
+  item.text = str;
+  item.value = val;
+  return item;
+}
+
+// формирование поля со списком 
+// параметры – массив со значениями элементов списка и элемент select
+
+const setSortSelect = (arr, sortSelect) => {
+  
+  // создаем OPTION Нет и добавляем ее в SELECT
+  sortSelect.append(createOption('Нет', 0));
+  // перебираем массив со значениями опций
+   arr.forEach((item, index) => {
+     // создаем OPTION из очередного ключа и добавляем в SELECT
+     // значение атрибута VALUE увеличиваем на 1, так как значение 0 имеет опция Нет
+      sortSelect.append(createOption(item, index + 1));
+  });
+}
+
+// формируем поля со списком для многоуровневой сортировки
+const setSortSelects = (data, dataForm) => { 
+
+  // выделяем ключи словаря в массив
+  const head = Object.keys(data);
+
+  // находим все SELECT в форме
+  const allSelect = dataForm.getElementsByTagName('select');
+  
+  for(const item of dataForm.elements){
+      // формируем очередной SELECT
+      setSortSelect(head, item);
+  
+      // САМОСТОЯТЕЛЬНО все SELECT, кроме первого, сделать неизменяемым
+      const allExceptFirst = [...allSelect].slice(1);
+      allExceptFirst.forEach(select => select.disabled = true)
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  setSortSelects(buildings[0], sort)
+});
+
+
+// настраиваем поле для следующего уровня сортировки
+const changeNextSelect = (curSelect, nextSelectId) => {
+    
+  let nextSelect = document.getElementById(nextSelectId);
+  
+  nextSelect.disabled = false;
+  
+  // в следующем SELECT выводим те же option, что и в текущем
+  nextSelect.innerHTML = curSelect.innerHTML;
+  
+  // удаляем в следующем SELECT уже выбранную в текущем опцию
+  // если это не первая опция - отсутствие сортировки
+  if (curSelect.value != 0) {
+     nextSelect.remove(curSelect.value);
+  } else {
+      nextSelect.disabled = true;
+  }
+}
+
+const fieldsFirst = document.getElementById('fieldsFirst');
+
+fieldsFirst.addEventListener('change', (event) => {
+  changeNextSelect(event.currentTarget, 'fieldsSecond');
 })
